be6bffaade865cbe1fc19493a7d0288d107f0dc1
FireFeuer <92076566+FireFeuer@users.noreply.github.com>
2024-03-27 12:25:03 +0500

    Delete faceIdentifierAPI directory

--- faceIdentifierAPI/faceIdentifierAPI/faceIdentifierAPI/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import UploadImageView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('upload/', UploadImageView.as_view(), name='upload'),
-]
