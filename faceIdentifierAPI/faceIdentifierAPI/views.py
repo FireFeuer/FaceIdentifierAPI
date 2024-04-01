246cc5a123dfa7d70ac42d748d34bd9cb03edf02
FireFeuer <92076566+FireFeuer@users.noreply.github.com>
2024-04-01 07:32:24 +0300

    теперь код возвращает строки, а не статусы

--- faceIdentifierAPI/faceIdentifierAPI/views.py
+++ faceIdentifierAPI/faceIdentifierAPI/views.py
@@ -9,8 +9,12 @@ def upload_image(request):
     serializer = ImageSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return "Прекрасный мир!"
+    else:
+        return "Не получилось"
+
+
+
     
 
  
